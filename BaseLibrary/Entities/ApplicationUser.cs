47b112fc6fac33c84efe1428bad127a0b6177626
Luiz Araujo <arauldesign@gmail.com>
2025-03-09 18:13:29 -0300

    feature: fill application user informations

--- BaseLibrary/Entities/ApplicationUser.cs
+++ BaseLibrary/Entities/ApplicationUser.cs
@@ -3,8 +3,23 @@
     public class ApplicationUser 
     {
         public int Id { get; private set; }
-        public string? Name { get; private set; }
+        public string? Fullname { get; private set; }
         public string? Email { get; private set; }
         public string? Password { get; private set; }
+
+        public void FullnameFill(string fullname)
+        {
+            Fullname = fullname;
+        }
+
+        public void EmailFill(string fullname)
+        {
+            Fullname = fullname;
+        }
+
+        public void PasswordFill(string fullname)
+        {
+            Fullname = fullname;
+        }
     }
 }
