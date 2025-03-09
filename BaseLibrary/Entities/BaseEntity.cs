e0296bea1b3990b0c4b91869ab61785b66f6aadb
Luiz Araujo <arauldesign@gmail.com>
2025-03-09 18:37:27 -0300

    fix: null

--- BaseLibrary/Entities/BaseEntity.cs
+++ BaseLibrary/Entities/BaseEntity.cs
@@ -5,7 +5,7 @@ namespace BaseLibrary.Entities
     public class BaseEntity
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
 
         // Relação : um para muitos
         [JsonIgnore]
