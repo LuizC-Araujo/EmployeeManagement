a86cb96f4d1794941778b598e9727dc3c2b833cb
Luiz Araujo <arauldesign@gmail.com>
2025-03-09 13:26:32 -0300

    feature: adding application user entity

---
+++ BaseLibrary/Entities/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿namespace BaseLibrary.Entities
+{
+    public class ApplicationUser 
+    {
+        public int Id { get; private set; }
+        public string? Name { get; private set; }
+        public string? Email { get; private set;
+        public string? Password { get; private set; }
+    }
+}
