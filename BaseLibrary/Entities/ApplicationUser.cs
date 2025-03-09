46fc57fb899a1c521e6e0553ae4ed689230aa2a3
Luiz Araujo <arauldesign@gmail.com>
2025-03-09 14:02:44 -0300

    fix: prop application user

--- BaseLibrary/Entities/ApplicationUser.cs
+++ BaseLibrary/Entities/ApplicationUser.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; private set; }
         public string? Name { get; private set; }
-        public string? Email { get; private set;
+        public string? Email { get; private set; }
         public string? Password { get; private set; }
     }
 }
