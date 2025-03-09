962a58f5b64b11057defa9d3b90de661f647f210
Luiz Araujo <arauldesign@gmail.com>
2025-03-09 18:13:48 -0300

    feature: adding jwt info in a class

---
+++ ServerLibrary/Helpers/JwtSection.cs
@@ -0,0 +1,9 @@
+﻿namespace ServerLibrary.Helpers
+{
+    public class JwtSection
+    {
+        public string? Key { get; private set; }
+        public string? Issuer { get; private set; }
+        public string? Audience { get; private set; }
+    }
+}
