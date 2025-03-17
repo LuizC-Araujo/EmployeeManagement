4c5900960f88e94bc626fac383558596cb3ab76e
Luiz Araujo <arauldesign@gmail.com>
2025-03-16 23:41:34 -0300

    fix: migration adjusts

--- ServerLibrary/Data/AppDbContext.cs
+++ ServerLibrary/Data/AppDbContext.cs
@@ -13,5 +13,12 @@ namespace ServerLibrary.Data
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
         public DbSet<UserRole> UserRoles { get; set; }
         public DbSet<SystemRole> SystemRoles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ApplicationUser>()
+                .Property(u => u.Id)
+                .ValueGeneratedOnAdd();  // Garante que o EF Core gere o ID automaticamente
+        }
     }
  }
