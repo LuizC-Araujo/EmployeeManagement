4d48cab6aa7eb4974f5cfa0e1eaac10620553547
Luiz Araujo <arauldesign@gmail.com>
2025-03-25 18:22:08 -0300

    feature: implementing create in user account service

--- ClientLibrary/Services/Implementations/UserAccountService.cs
+++ ClientLibrary/Services/Implementations/UserAccountService.cs
@@ -1,14 +1,21 @@
 ﻿using BaseLibrary.DTOs;
 using BaseLibrary.Responses;
+using ClientLibrary.Helpers;
 using ClientLibrary.Services.Contracts;
+using System.Net.Http.Json;
 
 namespace ClientLibrary.Services.Implementations
 {
-    public class UserAccountService : IUserAccountService
+    public class UserAccountService(GetHttpClient getHttpClient) : IUserAccountService
     {
-        public Task<GeneralResponse> CreateAsync(Register user)
+        public const string AuthUrl = "api/authentication";
+        public async Task<GeneralResponse> CreateAsync(Register user)
         {
-            throw new NotImplementedException();
+            var httpClient = getHttpClient.GetPublicHttpClient();
+            var result = await httpClient.PostAsJsonAsync($"{AuthUrl}/register", user);
+            if (!result.IsSuccessStatusCode) return new GeneralResponse(false, "Ocorreu um erro");
+
+            return await result.Content.ReadFromJsonAsync<GeneralResponse>()!;
         }
 
         public Task<LoginResponse> SignInAsync(Login user)
